f6d80bcd749d835a2c46aa5e5aa2c64f392dab85
asusGuy94 <asusGuy94.1@gmail.com>
2020-06-22 13:26:08 +0200

    Added more video-search filter options

--- json/video.search.php
+++ json/video.search.php
@@ -1,10 +1,10 @@
 <?php
-include('../_class.php');
-$basic = new Basic();
-$video = new Video();
-global $pdo;
-$sql = "
-            SELECT videos.path, videos.id AS videoID, videos.name AS video, videoalias.name AS alias, videos.franchise, videos.noStar, attributes.name AS attribute, categories.name AS category, videos.episode, videos.cen
+    include('../_class.php');
+    $basic = new Basic();
+    $video = new Video();
+    global $pdo;
+    $sql = "
+            SELECT videos.path, videos.height, videos.id AS videoID, videos.name AS video, videoalias.name AS alias, videos.date_published, videos.franchise, videos.noStar, attributes.name AS attribute, categories.name AS category, videos.episode, videos.cen
             FROM videos
               LEFT JOIN videoalias ON videos.id = videoalias.videoID
               LEFT JOIN videostars ON videostars.videoID = videos.id
@@ -14,7 +14,7 @@ $sql = "
               LEFT JOIN categories ON videocategories.categoryID = categories.id
             
           UNION ALL
-            SELECT videos.path, videos.id AS videoID, videos.name AS video, videoalias.name AS alias, videos.franchise, videos.noStar, attributes.name AS attribute, categories.name AS category, videos.episode, videos.cen
+            SELECT videos.path, videos.height, videos.id AS videoID, videos.name AS video, videoalias.name AS alias, videos.date_published, videos.franchise, videos.noStar, attributes.name AS attribute, categories.name AS category, videos.episode, videos.cen
             FROM videos
               LEFT JOIN videoalias ON videos.id = videoalias.videoID
               LEFT JOIN bookmarks ON videos.id = bookmarks.videoID
@@ -22,105 +22,102 @@ $sql = "
               LEFT JOIN attributes ON bookmarkattributes.attributeID = attributes.id
               LEFT JOIN categories ON bookmarks.categoryID = categories.id
               
-          ORDER BY franchise, episode
+          ORDER BY video, path
           ";
-
-$query = $pdo->prepare($sql);
-$query->execute();
-$result = $query->fetchAll(PDO::FETCH_OBJ);
+    
+    $query = $pdo->prepare($sql);
+    $query->execute();
+    $result = $query->fetchAll(PDO::FETCH_OBJ);
 
 // Initialize Arrays
-$attribute_arr = [];
-$category_arr = [];
-$alias_arr = [];
-
-print '{';
-print '"videos": [';
-for ($i = 0, $len = count($result); $i < $len; $i++) {
-	$videoID = $result[$i]->videoID;
-	$videoName = $result[$i]->video;
-	$noStar = $result[$i]->noStar;
-	$franchise = $result[$i]->franchise;
-	$attributeName = $result[$i]->attribute;
-	$categoryName = $result[$i]->category;
-	$fileName = $result[$i]->path;
-	$aliasName = $result[$i]->alias;
-	$cen = $result[$i]->cen;
-
-	$nextIsDuplicate = ($i < $len - 1 && ($result[$i + 1]->video == $videoName));
-	$prevIsDuplicate = ($i > 0 && ($result[$i - 1]->video == $videoName));
-
-
-	if (!$prevIsDuplicate) { // first video of the bunch
-		print '{';
-
-		$thumbnail = "images/videos/$videoID-" . THUMBNAIL_RES . ".jpg";
-
-		print "\"videoID\": $videoID,";
-		print "\"noStar\": " . intval($noStar) . ",";
-		print "\"cen\": \"$cen\",";
-		print "\"franchise\": \"$franchise\",";
-		print "\"videoName\": \"$videoName\",";
-		print "\"thumbnail\": \"$thumbnail\",";
-	}
-
-	// Attribute INIT
-	if (!is_null($attributeName)) {
-		if (!in_array($attributeName, $attribute_arr)) array_push($attribute_arr, $attributeName);
-	}
-
-	// Category INIT
-	if (!is_null($categoryName)) {
-		if (!in_array($categoryName, $category_arr)) array_push($category_arr, $categoryName);
-	}
-
-	// Alias INIT
-	if (!is_null($aliasName)) {
-		if (!in_array($aliasName, $alias_arr)) array_push($alias_arr, $aliasName);
-	}
-
-	if (((!$nextIsDuplicate && $prevIsDuplicate) || (!$nextIsDuplicate && !$prevIsDuplicate)) && $videoName) { // last video of the bunch
-		// Attribute
-		print '"attribute": [';
-		if (count($attribute_arr)) {
-			for ($j = 0; $j < count($attribute_arr); $j++) {
-				print "\"$attribute_arr[$j]\"";
-				if ($j < count($attribute_arr) - 1) print ',';
-			}
-		}
-		print '],';
-
-		// Category
-		print '"category": [';
-		if (count($category_arr)) {
-			for ($j = 0; $j < count($category_arr); $j++) {
-				print "\"$category_arr[$j]\"";
-				if ($j < count($category_arr) - 1) print ',';
-			}
-		}
-		print '],';
-
-		// Alias
-		print '"alias": [';
-		if (count($alias_arr)) {
-			for ($j = 0; $j < count($alias_arr); $j++) {
-				print "\"$alias_arr[$j]\"";
-				if ($j < count($alias_arr) - 1) print ',';
-			}
-		}
-		print ']';
-
-		/* Reset Arrays */
-		$attribute_arr = [];
-		$category_arr = [];
-		$alias_arr = [];
-	}
-
-	if (!$nextIsDuplicate) {
-		if ($i < $len - 1) print '},';
-		else print '}';
-	}
-}
-
-print ']';
-print '}';
+    $attribute_arr = [];
+    $category_arr = [];
+    $alias_arr = [];
+    
+    print '{';
+    print '"videos": [';
+    for ($i = 0, $len = count($result); $i < $len; $i++) {
+        $videoID = $result[$i]->videoID;
+        $videoName = $result[$i]->video;
+        $datePublished = $result[$i]->date_published;
+        $noStar = $result[$i]->noStar;
+        $franchise = $result[$i]->franchise;
+        $attributeName = $result[$i]->attribute;
+        $categoryName = $result[$i]->category;
+        $fileName = $result[$i]->path;
+        $aliasName = $result[$i]->alias;
+        $cen = $result[$i]->cen;
+        $quality = $result[$i]->height;
+        
+        $nextIsDuplicate = ($i < $len - 1 && ($result[$i + 1]->video == $videoName));
+        $prevIsDuplicate = ($i > 0 && ($result[$i - 1]->video == $videoName));
+        
+        if (!$prevIsDuplicate) { // first video of the bunch
+            print '{';
+            
+            $thumbnail = "images/videos/$videoID-" . THUMBNAIL_RES . ".jpg";
+            
+            print "\"videoID\": $videoID,";
+            print "\"noStar\": " . intval($noStar) . ",";
+            print "\"cen\": \"$cen\",";
+            print "\"franchise\": \"$franchise\",";
+            print "\"videoName\": \"$videoName\",";
+            print "\"datePublished\": \"$datePublished\",";
+            print "\"thumbnail\": \"$thumbnail\",";
+            print "\"plays\": \"" . getPlays($videoID) . "\",";
+            print "\"existing\": \"" . (int)Basic::file_check($fileName, $videoID, false) . "\",";
+            print "\"quality\": \"$quality\",";
+        }
+        
+        // Init Arrays
+        initArray($attribute_arr, $attributeName);
+        initArray($category_arr, $categoryName);
+        initArray($alias_arr, $aliasName);
+        
+        if (!$nextIsDuplicate) {
+            /* Build Arrays */
+            handleArray('attribute', $attribute_arr);
+            handleArray('category', $category_arr);
+            handleArray('alias', $alias_arr, true);
+            
+            if ($i < $len - 1) print '},';
+            else print '}';
+            
+            // Reset Arrays
+            $attribute_arr = [];
+            $category_arr = [];
+            $alias_arr = [];
+        }
+    }
+    
+    print ']';
+    print '}';
+    
+    function initArray(&$arr, $item)
+    {
+        if (!is_null($item)) {
+            if (!in_array($item, $arr)) array_push($arr, $item);
+        }
+    }
+    
+    function handleArray($label, $arr, $last = false)
+    {
+        print '"' . $label . '": [';
+        if (count($arr)) {
+            for ($j = 0; $j < count($arr); $j++) {
+                print "\"$arr[$j]\"";
+                if ($j < count($arr) - 1) print ',';
+            }
+        }
+        print ']';
+        if (!$last) print ',';
+    }
+    
+    function getPlays($id)
+    {
+        global $pdo;
+        $query = $pdo->prepare("SELECT COUNT(*) AS total FROM plays WHERE videoID = :videoID");
+        $query->bindParam(':videoID', $id);
+        $query->execute();
+        return $query->fetch()['total'];
+    }

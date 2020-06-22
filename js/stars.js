4b09aecdebaaeba6f400e6f769eaa05302746036
asusGuy94 <asusGuy94.1@gmail.com>
2020-06-22 13:11:31 +0200

    Removed semicolons & Improved code readability

--- js/stars.js
+++ js/stars.js
@@ -1,16 +1,15 @@
-function editStar(starID){
-    let arguments = 'starID=' + starID;
-    ajax('edit_star.php', arguments);
+function editStar(starID) {
+    ajax('edit_star.php', 'starID=' + starID)
 }
 
 function ajax(page, params) {
-    let url = page + '?' + params;
+    let url = page + '?' + params
 
-    let xhr = new XMLHttpRequest();
-    xhr.open('GET', url);
-    xhr.send();
+    let xhr = new XMLHttpRequest()
+    xhr.open('GET', url)
+    xhr.send()
     xhr.onreadystatechange = function () {
-        if (this.readyState === 4 && this.status === 200) reloadPage();
+        if (this.readyState === 4 && this.status === 200) reloadPage()
     }
 }
 

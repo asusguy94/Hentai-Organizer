24a59b80d6bf3de96f592e5386efc9c0ed264831
asusGuy94 <asusGuy94.1@gmail.com>
2020-08-02 18:38:11 +0200

    Added esc-key for popup

--- src/components/modal.jsx
+++ src/components/modal.jsx
@@ -2,14 +2,24 @@ import React, { Component } from 'react'
 
 import './styles/modal.scss'
 
-// TODO ESC-key press
 class Modal extends Component {
+    constructor(props) {
+        super(props)
+        this.escFunc = this.escFunc.bind(this)
+    }
+
+    escFunc(e) {
+        if (this.props.visible) {
+            if (e.keyCode === 27) this.props.onClose()
+        }
+    }
+
     render() {
         return (
             <React.Fragment>
                 {this.props.visible && (
                     <div id='modal' className='card'>
-                        <div className='card-body py-1'>
+                        <div className='card-body'>
                             <h2 className='card-title'>{this.props.title}</h2>
                             <div className='content'>{this.props.children}</div>
                             <div className='actions'>
@@ -22,6 +32,14 @@ class Modal extends Component {
                 )}
             </React.Fragment>
         )
+    }
+
+    componentDidMount() {
+        document.addEventListener('keydown', this.escFunc, false)
+    }
+
+    componentWillUnmount() {
+        document.removeEventListener('keydown', this.escFunc, false)
     }
 }
 

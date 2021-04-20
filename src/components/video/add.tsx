4435b7a553688a65e88be78332f2761d94f1abb5
asusguy94 <asusGuy94.1@gmail.com>
2021-04-20 15:03:13 +0200

    Updated Import page
    Previous version was partially broken, so I've made a reworked version
    This version does not allow for changes to data during import

--- src/components/video/add.tsx
+++ src/components/video/add.tsx
@@ -2,6 +2,8 @@ import { useState, useEffect } from 'react'
 
 import Axios from 'axios'
 
+import Loader from '../loader/loader'
+
 import config from '../config.json'
 
 interface IVideo {
@@ -21,45 +23,62 @@ const AddVideoPage = () => {
 	}, [])
 
 	return (
-		<div className='row col justify-content-center'>
-			<h1 className='text-center'>Add Videos</h1>
+		<div className='col-12'>
+			<h2 className='text-center'>Import Videos</h2>
 			{loaded ? (
-				<>
-					<form>
-						{videos.map((video, key) => (
-							<div key={key}>
-								<CustomInput label='Path' item={video} />
-								<CustomInput label='Name' item={video} />
-								<CustomInput label='Franchise' item={video} />
-								<CustomInput label='Episode' item={video} />
+				!videos.length ? (
+					<div className='text-center'>
+						<Button
+							label='Generate Thumbnails'
+							callback={() => Axios.post(`${config.source}/generate/thumb`)}
+						/>
+						<Button
+							label='Generate Metadata'
+							callback={() => Axios.post(`${config.source}/generate/meta`)}
+						/>
+						<Button label='Generate VTT' disabled={true} />
+					</div>
+				) : (
+					<>
+						<table className='table table-sm table-striped'>
+							<thead>
+								<tr>
+									<th>episode</th>
+									<th>franchise</th>
+									<th>title</th>
+									<th>path</th>
+								</tr>
+							</thead>
 
-								<hr />
-							</div>
-						))}
-					</form>
+							<tbody>
+								{videos.map((video) => {
+									return (
+										<tr key={video.path}>
+											<td>{video.episode}</td>
+											<td>{video.franchise}</td>
+											<td>{video.name}</td>
+											<td>{video.path}</td>
+										</tr>
+									)
+								})}
+							</tbody>
+						</table>
 
-					{videos.length ? (
-						<div
-							className='btn btn-primary'
-							onClick={(e) => {
-								Axios.post(`${config.source}/video/add`, { videos }).then(() => {
-									window.location.reload()
-								})
-							}}
-						>
-							Submit
-						</div>
-					) : (
-						<>
+						<div className='text-center'>
 							<Button
-								label='Generate Thumbnails'
-								callback={() => Axios.post(`${config.source}/generate/thumb`)}
+								label='Add Videos'
+								callback={() =>
+									Axios.post(`${config.source}/video/add`, { videos }).then(() => {
+										window.location.reload()
+									})
+								}
 							/>
-							<Button label='Generate WebVTT' disabled={true} />
-						</>
-					)}
-				</>
-			) : null}
+						</div>
+					</>
+				)
+			) : (
+				<Loader />
+			)}
 		</div>
 	)
 }
@@ -83,23 +102,6 @@ const Button = ({ label, callback = () => {}, disabled = false }: IButton) => {
 	return (
 		<div className={`btn btn-info mx-1 ${isDisabled ? 'disabled' : ''}`} onClick={clickHandler}>
 			{label}
-		</div>
-	)
-}
-
-const CustomInput = ({ label, item }: any) => {
-	const lowerLabel = label.toLowerCase()
-	const data = item[lowerLabel]
-
-	return (
-		<div className='form-group'>
-			<label htmlFor={lowerLabel}>{label}: </label>
-			<input
-				id={lowerLabel}
-				className='form-control'
-				defaultValue={data}
-				style={{ width: `${data.length < 4 ? 4 : data.length}ch` }}
-			/>
 		</div>
 	)
 }

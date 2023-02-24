4e58db5882f7c73f9bf537afb6b65c82a293209e
asusGuy94 <asusguy94.1@gmail.com>
2023-02-24 21:31:49 +0100

    Migrated to nextjs own font-service

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,7 @@
 import Head from 'next/head'
+import Script from 'next/script'
 import type { AppProps } from 'next/app'
+import { Roboto } from 'next/font/google'
 
 import { Container, CssBaseline } from '@mui/material'
 
@@ -8,6 +10,12 @@ import NavBar from '@components/navbar'
 import '@styles/globals.scss'
 import 'plyr/dist/plyr.css'
 
+const roboto = Roboto({
+  weight: ['300', '400', '500', '700'],
+  display: 'swap',
+  subsets: ['latin']
+})
+
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
@@ -15,10 +23,13 @@ export default function MyApp({ Component, pageProps }: AppProps) {
         <title>Hentai</title>
       </Head>
 
+      <Script async src='https://pro.fontawesome.com/releases/v5.15.4/js/all.js' />
+      <Script async src='https://pro.fontawesome.com/releases/v5.15.4/js/fontawesome.js' />
+
       <CssBaseline />
 
       <NavBar />
-      <Container component='main' maxWidth={false}>
+      <Container component='main' maxWidth={false} className={roboto.className}>
         <Component {...pageProps} />
       </Container>
     </>

be84446903fbacbc720768f4e48285f5384a9613
asusGuy94 <asusguy94.1@gmail.com>
2023-04-15 09:40:25 +0200

    Added clamping-method

--- utils/shared.ts
+++ utils/shared.ts
@@ -5,3 +5,13 @@ export const getUnique = <T>(arr: T[], prop?: keyof T): T[] => {
 
   return [...new Set(arr)]
 }
+
+export function clamp(value: number, minOrMax: number, max?: number): number {
+  if (max === undefined) {
+    // min was not supplied, use 0 as default value
+    return clamp(value, 0, minOrMax)
+  }
+
+  // min was supplied, use regular clamp
+  return Math.min(Math.max(value, minOrMax), max)
+}

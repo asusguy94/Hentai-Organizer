e1e4abf746a4faaf81afb7c48e3b07ce76d7410b
asusGuy94 <asusguy94.1@gmail.com>
2024-03-08 22:32:16 +0100

    Optimized vtt-performance

--- src/pages/api/generate/vtt.ts
+++ src/pages/api/generate/vtt.ts
@@ -10,12 +10,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   if (req.method === 'POST') {
     const videos = await db.video.findMany({ where: { duration: { gt: 0 }, height: { gt: 0 } } })
 
-    socket.trigger('ffmpeg', 'vtt', { progress: 0 })
-    for (let i = 0; i < videos.length; i++) {
-      const video = videos[i]
-
-      const { progress } = getProgress(i, videos.length)
-
+    const generatePath = (video: (typeof videos)[number]) => {
       const videoPath = `videos/${video.path}`
       const imagePath = `vtt/${video.id}.jpg`
       const vttPath = `vtt/${video.id}.vtt`
@@ -24,13 +19,31 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       const absoluteImagePath = `./media/${imagePath}`
       const absoluteVttPath = `./media/${vttPath}`
 
-      if (
-        (await fileExists(absoluteVideoPath)) &&
-        (!(await fileExists(absoluteVttPath)) || !(await fileExists(absoluteImagePath)))
-      ) {
-        socket.trigger('ffmpeg', 'vtt', { progress })
-        await extractVtt(absoluteVideoPath, absoluteImagePath, video.id)
+      return {
+        videoPath: absoluteVideoPath,
+        imagePath: absoluteImagePath,
+        vttPath: absoluteVttPath
       }
+    }
+
+    const missingVtt: typeof videos = []
+    for await (const video of videos) {
+      const { imagePath, videoPath, vttPath } = generatePath(video)
+
+      if ((await fileExists(videoPath)) && (!(await fileExists(vttPath)) || !(await fileExists(imagePath)))) {
+        missingVtt.push(video)
+      }
+    }
+
+    socket.trigger('ffmpeg', 'vtt', { progress: 0 })
+    for (let i = 0; i < missingVtt.length; i++) {
+      const video = missingVtt[i]
+
+      const { progress } = getProgress(i, missingVtt.length)
+      const { imagePath, videoPath } = generatePath(video)
+
+      socket.trigger('ffmpeg', 'vtt', { progress })
+      await extractVtt(videoPath, imagePath, video.id)
     }
     socket.trigger('ffmpeg', 'vtt', { progress: 1 })
 

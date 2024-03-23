6adb65cd83cf3244c9df76420b0707869a054702
asusGuy94 <asusguy94.1@gmail.com>
2024-03-23 14:33:22 +0100

    Added debounce to `<Spinner />`

--- src/components/spinner/index.tsx
+++ src/components/spinner/index.tsx
@@ -1,5 +1,21 @@
+import { useEffect, useState } from 'react'
+
 import styles from './loader.module.css'
 
-export default function Spinner() {
+type SpinnerProps = {
+  delay?: number
+}
+
+export default function Spinner({ delay = 0 }: SpinnerProps) {
+  const [showSpinner, setShowSpinner] = useState(false)
+
+  useEffect(() => {
+    const timer = setTimeout(() => setShowSpinner(true), delay)
+
+    return () => clearTimeout(timer)
+  }, [delay])
+
+  if (!showSpinner) return false
+
   return <div id={styles.loader}></div>
 }

57c9b2d81d69acaa98570eaa7ee927b598719023
asusGuy94 <asusguy94.1@gmail.com>
2024-03-30 16:49:48 +0100

    Updated package

--- src/components/error.tsx
+++ src/components/error.tsx
@@ -1,12 +1,14 @@
-type ErrorProps = {
-  error: Error
-}
+import { ErrorComponent as DefaultErrorComponent, ErrorComponentProps } from '@tanstack/react-router'
 
-export default function Error({ error }: ErrorProps) {
-  return (
-    <div>
-      <h1>{error.message}</h1>
-      <pre>{error.stack}</pre>
-    </div>
-  )
+export default function ErrorComponent({ error }: ErrorComponentProps) {
+  if (error instanceof Error && import.meta.env.DEV) {
+    return (
+      <div>
+        <h1>{error.message}</h1>
+        <pre>{error.stack}</pre>
+      </div>
+    )
+  }
+
+  return <DefaultErrorComponent error={error} />
 }
